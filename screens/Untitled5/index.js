82bbff1d7c2c4038cb733414cc4cbd388af4371f
Aline Tavares <alaisgomes@gmail.com>
2023-05-19 11:57:04 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled5/index.js
@@ -0,0 +1,24 @@
+import { ImageBackground } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled5 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><ImageBackground style={styles.VyLyVhiZ} source={require("./kgdi7n00kk481.png")} resizeMode="cover"></ImageBackground></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  VyLyVhiZ: {
+    width: 107,
+    height: 62
+  }
+});
+export default Untitled5;
